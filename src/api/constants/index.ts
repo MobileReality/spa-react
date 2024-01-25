e748e1292499e1e27b5b648982e78825b3be00eb
MykhailoButkevych-MR <mykhailo@mobilereality.pl>
2024-01-25 13:36:16 +0400

    feat: auth0 provider, types

--- src/api/constants/index.ts
+++ src/api/constants/index.ts
@@ -1,1 +1,1 @@
-export const API_URL = `https://${process.env.REACT_APP_REALM}`
+export const API_URL = `https://${import.meta.env.REACT_APP_REALM}`
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,17 +4,30 @@ import '#locales/i18n'
 import { Toaster } from 'react-hot-toast'
 import { useTranslation } from 'react-i18next'
 import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+import { API_URL } from '#api/constants'
 
 // import.meta.env
 export const App = () => {
   const { t } = useTranslation('common')
 
   return (
-    <>
+    <Auth0Provider
+      domain={import.meta.env.REACT_APP_AUTH0_API_URL}
+      clientId={import.meta.env.REACT_APP_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: API_URL,
+        scope: 'offline_access openid profile email address phone',
+      }}
+      useRefreshTokens
+      onRedirectCallback={() => {}}
+    >
       <BrowserRouter>
         <div>{t('test')}</div>
       </BrowserRouter>
       <Toaster />
-    </>
+    </Auth0Provider>
   )
 }
